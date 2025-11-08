a3bf00e47a7dd05a944d58a8657b3b526d537c21
dlgutierrez <loribeth90@hotmail.com>
2025-11-08 19:57:50 +0000

    fastapi readme

--- fast_api_example/main.py
+++ fast_api_example/main.py
@@ -15,4 +15,4 @@ async def read_item(item_id: int, q: str = None):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=5000, log_level="info")
+    uvicorn.run("main:app", host="localhost", port=5000, log_level="info")
